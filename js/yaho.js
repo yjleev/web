2c42c8b72aabce0ef14adc39002126f5d5f464cb
yjleev <sjlee081010@gmail>
2024-08-13 15:31:26 +0900

    yaho

---
+++ js/yaho.js
@@ -0,0 +1,6 @@
+const img1 = document.getElementsByClassName('img1')
+const img2 = document.getElementsByClassName('img2')
+const img3 = document.getElementsByClassName('img3')
+const img4 = document.getElementsByClassName('img4')
+const img5 = document.getElementsByClassName('img5')
+
